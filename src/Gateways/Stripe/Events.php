7843fddf3f17671296158e16e212889afc63da98
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-12-14 15:02:31 -0600

    Add events class to Stripe

---
+++ src/Gateways/Stripe/Events.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Shoperti\PayMe\Gateways\Stripe;
+
+use InvalidArgumentException;
+use Shoperti\PayMe\Gateways\AbstractApi;
+use Shoperti\PayMe\Contracts\EventInterface;
+
+/**
+ * This is the conekta events class.
+ *
+ * @author joseph.cohen@dinkbit.com
+ */
+ class Events extends AbstractApi implements EventInterface
+{
+    /**
+     * Find all events.
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function all()
+    {
+        return $this->commit('get', $this->buildUrlFromString('events'));
+    }
+
+    /**
+     * Find an event by its id.
+     *
+     * @param int|string $id
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function find($id = null)
+    {
+        if (!$id) {
+            throw new InvalidArgumentException('We need an id');
+        }
+
+        return $this->commit('get', $this->buildUrlFromString("events/{$id}"));
+    }
+}
