9a34dd12afab236fd869617e62fbbc8df878bb37
Arturo Rodríguez <arturo.rodriguez@dinkbit.com>
2016-08-08 14:11:59 -0500

    Add manual gateway

---
+++ src/Gateways/Manual/Charges.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Shoperti\PayMe\Gateways\Manual;
+
+use Shoperti\PayMe\Contracts\ChargeInterface;
+use Shoperti\PayMe\Gateways\AbstractApi;
+use Shoperti\PayMe\Status;
+
+/**
+ * This is the manual charges class.
+ *
+ * @author Arturo Rodríguez <arturo.rodriguez@dinkbit.com>
+ */
+class Charges extends AbstractApi implements ChargeInterface
+{
+    /**
+     * Create a charge.
+     *
+     * @param int|float $amount
+     * @param mixed     $payment
+     * @param string[]  $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function create($amount, $payment, $options = [])
+    {
+        $params = [
+            'type'   => 'charge',
+            'amount' => $amount,
+            'status' => new Status('pending'),
+        ];
+
+        return $this->gateway->commit(null, null, $params);
+    }
+
+    /**
+     * Complete a charge.
+     *
+     * @param string[] $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function complete($options = [])
+    {
+        $params = [
+            'type'   => 'charge',
+            'status' => new Status('paid'),
+        ];
+
+        return $this->gateway->commit(null, null, $params);
+    }
+
+    /**
+     * Refund a charge.
+     *
+     * @param int|float $amount
+     * @param string    $reference
+     * @param string[]  $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function refund($amount, $reference, array $options = [])
+    {
+        $params = [
+            'type'   => 'refund',
+            'amount' => $amount,
+            'status' => new Status('refunded'),
+        ];
+
+        return $this->gateway->commit(null, null, $params);
+    }
+}
---
+++ tests/Functional/ManualTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Shoperti\Tests\PayMe\Functional;
+
+use Shoperti\PayMe\PayMe;
+
+class ManualTest extends AbstractFunctionalTestCase
+{
+    /** @test */
+    public function it_should_create_a_new_manual_gateway()
+    {
+        $gateway = PayMe::make($this->credentials['manual']);
+
+        $this->assertInstanceOf('Shoperti\PayMe\Gateways\Manual\ManualGateway', $gateway->getGateway());
+        $this->assertInstanceOf('Shoperti\PayMe\Gateways\Manual\Charges', $gateway->charges());
+    }
+
+    /** @test */
+    public function is_should_succeed_to_perform_a_charge()
+    {
+        $gateway = PayMe::make($this->credentials['manual']);
+
+        $charge = $gateway->charges()->create(1000, 'payment');
+
+        $this->assertTrue($charge->success());
+        $this->assertEquals('charge', $charge->type());
+        $this->assertEquals('pending', $charge->status());
+    }
+
+    /** @test */
+    public function is_should_succeed_to_perform_a_completion()
+    {
+        $gateway = PayMe::make($this->credentials['manual']);
+
+        $charge = $gateway->charges()->complete(1000, 'payment');
+
+        $this->assertTrue($charge->success());
+        $this->assertEquals('charge', $charge->type());
+        $this->assertEquals('paid', $charge->status());
+    }
+
+    /** @test */
+    public function is_should_succeed_to_perform_a_refund()
+    {
+        $gateway = PayMe::make($this->credentials['manual']);
+
+        $charge = $gateway->charges()->refund(1000, 'reference');
+
+        $this->assertTrue($charge->success());
+        $this->assertEquals('refund', $charge->type());
+        $this->assertEquals('refunded', $charge->status());
+    }
+}
