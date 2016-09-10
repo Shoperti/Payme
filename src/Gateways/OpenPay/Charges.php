e77a527ba19456f87d4b7524bae758617bf6e16d
Arturo Rodríguez <arturo.rodriguez@dinkbit.com>
2016-09-09 20:48:01 -0500

    Add codebase for OpenPay gateway

---
+++ src/Gateways/OpenPay/Charges.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Shoperti\PayMe\Gateways\OpenPay;
+
+use BadMethodCallException;
+use Shoperti\PayMe\Contracts\ChargeInterface;
+use Shoperti\PayMe\Gateways\AbstractApi;
+use Shoperti\PayMe\Support\Arr;
+use Shoperti\PayMe\Support\Helper;
+
+/**
+ * This is the OpenPay charges class.
+ *
+ * @author Arturo Rodríguez <arturo.rodriguez@dinkbit.com>
+ */
+class Charges extends AbstractApi implements ChargeInterface
+{
+    /**
+     * Create a charge.
+     *
+     * @param int|float $amount
+     * @param mixed     $payment
+     * @param string[]  $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function create($amount, $payment, $options = [])
+    {
+        $params = [];
+
+        $params = $this->addPaymentMethod($params, $payment, $options);
+        $params = $this->addOrder($params, $amount, $options);
+        $params = $this->addCustomer($params, $options);
+
+        if (isset($options['monthly_installments']) && in_array($options['monthly_installments'], [3, 6, 9, 12])) {
+            $params['monthly_installments'] = $options['monthly_installments'];
+        }
+
+        return $this->gateway->commit('post', $this->gateway->buildUrlFromString('charges'), $params);
+    }
+
+    /**
+     * Complete a charge.
+     *
+     * @param string[] $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function complete($options = [])
+    {
+        throw new BadMethodCallException();
+    }
+
+    /**
+     * Refund a charge.
+     *
+     * @param int|float $amount
+     * @param string    $reference
+     * @param string[]  $options
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function refund($amount, $reference, array $options = [])
+    {
+        $url = sprintf($this->gateway->buildUrlFromString('charges').'/%s/refund', $reference);
+
+        return $this->gateway->commit('post', $url, [
+            'amount'      => $this->gateway->amount($amount),
+            'description' => Arr::get($options, 'description'),
+        ]);
+    }
+
+    /**
+     * Add payment method to request.
+     *
+     * @param string[] $params
+     * @param mixed    $payment
+     * @param string[] $options
+     *
+     * @return array
+     */
+    protected function addPaymentMethod(array $params, $payment, array $options)
+    {
+        if (is_string($payment)) {
+            // offline & SPEI
+            if (in_array($payment, ['store', 'bank_account'])) {
+                return array_merge($params, [
+                    'method' => $payment,
+                ]);
+            }
+
+            // dispersion
+            if (Helper::startsWith($payment, 'payee_')) {
+                //                $params['payee_id'] = $payment;
+            }
+
+            // cards
+            return array_merge($params, [
+                'method'            => 'card',
+                'source_id'         => $payment,
+                'device_session_id' => Arr::get($options, 'device_id'),
+            ]);
+        }
+
+        return $params;
+    }
+
+    /**
+     * Add order params to request.
+     *
+     * @param string[] $params
+     * @param int      $money
+     * @param string[] $options
+     *
+     * @return array
+     */
+    protected function addOrder(array $params, $money, array $options)
+    {
+        return array_merge($params, [
+            'amount'      => $this->gateway->amount($money),
+            'currency'    => Arr::get($options, 'currency', $this->gateway->getCurrency()),
+            'description' => Helper::ascii(Arr::get($options, 'description', 'PayMe Purchase')),
+            'order_id'    => Arr::get($options, 'reference'),
+        ]);
+    }
+
+    /**
+     * Add customer to request.
+     *
+     * @param string[] $params
+     * @param string[] $options
+     *
+     * @return array
+     */
+    protected function addCustomer(array $params, array $options)
+    {
+        $address = Arr::get($options, 'shipping_address', []);
+
+        $params['customer'] = [
+            'name'             => Arr::get($options, 'first_name', ''),
+            'last_name'        => Arr::get($options, 'last_name', ''),
+            'phone_number'     => Arr::get($options, 'phone', ''),
+            'email'            => Arr::get($options, 'email', ''),
+            'requires_account' => false,
+            'address'          => [
+                'city'         => Arr::get($address, 'city', ''),
+                'state'        => Arr::get($address, 'state', ''),
+                'line1'        => Arr::get($address, 'address1', ''),
+                'postal_code'  => Arr::get($address, 'zip', ''),
+                'line2'        => Arr::get($address, 'address2', ''),
+                'line3'        => '',
+                'country_code' => Arr::get($address, 'country', ''),
+            ],
+        ];
+
+        return $params;
+    }
+}
---
+++ src/Gateways/OpenPay/Webhooks.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Shoperti\PayMe\Gateways\OpenPay;
+
+use BadMethodCallException;
+use InvalidArgumentException;
+use Shoperti\PayMe\Contracts\WebhookInterface;
+use Shoperti\PayMe\Gateways\AbstractApi;
+
+/**
+ * This is the OpenPay events class.
+ *
+ * @author Joseph Cohen <joseph.cohen@dinkbit.com>
+ */
+class Webhooks extends AbstractApi implements WebhookInterface
+{
+    /**
+     * Find all events.
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function all()
+    {
+        return $this->gateway->commit('get', $this->gateway->buildUrlFromString('webhooks'));
+    }
+
+    /**
+     * Find a webhook by its id.
+     *
+     * @param int|string $id
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function find($id = null)
+    {
+        if (!$id) {
+            throw new InvalidArgumentException('We need an id');
+        }
+
+        return $this->gateway->commit('get', $this->gateway->buildUrlFromString('webhooks/'.$id));
+    }
+
+    /**
+     * Create a webhook.
+     *
+     * @param array $params
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function create($params = [])
+    {
+        return $this->gateway->commit('post', $this->gateway->buildUrlFromString('webhooks'), $params);
+    }
+
+    /**
+     * Update a webhook.
+     *
+     * @param array $params
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function update($params = [])
+    {
+        throw new BadMethodCallException();
+    }
+
+    /**
+     * Delete a webhook.
+     *
+     * @param int|string $id
+     *
+     * @return \Shoperti\PayMe\Contracts\ResponseInterface
+     */
+    public function delete($id)
+    {
+        return $this->gateway->commit('delete', $this->gateway->buildUrlFromString('webhooks/'.$id));
+    }
+}
