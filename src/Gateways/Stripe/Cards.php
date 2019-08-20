f3a04073593c8044543863eb7356b8614b33a9f4
Joe Cohen <joecohenr@gmail.com>
2019-08-20 22:56:15 +0000

    Apply fixes from StyleCI

--- src/Gateways/Stripe/Cards.php
+++ src/Gateways/Stripe/Cards.php
@@ -7,10 +7,10 @@ use Shoperti\PayMe\Gateways\AbstractApi;
 use Shoperti\PayMe\Support\Arr;
 
 /**
-  * This is the Stripe cards class.
-  *
-  * @author Joseph Cohen <joseph.cohen@dinkbit.com>
-  */
+ * This is the Stripe cards class.
+ *
+ * @author Joseph Cohen <joseph.cohen@dinkbit.com>
+ */
  class Cards extends AbstractApi implements CardInterface
  {
      /**
