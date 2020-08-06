ebef25d191777797bbeae3df71d70d2617030b34
Arturo Rodriguez <arturo@fugadigital.com>
2020-08-06 16:39:47 -0500

    Update test payload

--- tests/Unit/StripeGatewayTest.php
+++ tests/Unit/StripeGatewayTest.php
@@ -255,19 +255,6 @@ class StripeGatewayTest extends AbstractTestCase
     {
         return [
             'error' => [
-                'type'           => 'card_error',
-                'code'           => 'account_number_invalid',
-                'decline_code'   => false,
-                'message'        => 'Msg for user',
-                'param'          => 'If the error is parameter-specific, the parameter related to the error. For example, you can use this to display a message near the correct form field.',
-                'payment_intent' => [
-                    'id' => 'uniqueid',
-                ],
-            ],
-        ];
-
-        [
-            'error' => [
                 'message' => 'Invalid currency: usdz. Stripe currently supports these currencies: usd, aed, afn, all, amd, ang, aoa, ars, aud, awg, azn, bam, bbd, bdt, bgn, bif, bmd, bnd, bob, brl, bsd, bwp, bzd, cad, cdf, chf, clp, cny, cop, crc, cve, czk, djf, dkk, dop, dzd, egp, etb, eur, fjd, fkp, gbp, gel, gip, gmd, gnf, gtq, gyd, hkd, hnl, hrk, htg, huf, idr, ils, inr, isk, jmd, jpy, kes, kgs, khr, kmf, krw, kyd, kzt, lak, lbp, lkr, lrd, lsl, mad, mdl, mga, mkd, mmk, mnt, mop, mro, mur, mvr, mwk, mxn, myr, mzn, nad, ngn, nio, nok, npr, nzd, pab, pen, pgk, php, pkr, pln, pyg, qar, ron, rsd, rub, rwf, sar, sbd, scr, sek, sgd, shp, sll, sos, srd, std, szl, thb, tjs, top, try, ttd, twd, tzs, uah, ugx, uyu, uzs, vnd, vuv, wst, xaf, xcd, xof, xpf, yer, zar, zmw, eek, lvl, svc, vef, ltl',
                 'param'   => 'currency',
                 'type'    => 'invalid_request_error',
